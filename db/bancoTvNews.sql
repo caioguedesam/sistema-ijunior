898044e5d421e2c11ef7e643a3ba8663a9f763ba
Guilherme Miranda <gui.miranda.13@gmail.com>
2018-10-29 14:04:41 -0300

    adding database script

---
+++ db/bancoTvNews.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE tvnews;
+
+CREATE TABLE Movie(
+	idMovie INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	releaseYear INT NOT NULL,
+	runningTime INT NOT NULL,
+	genre VARCHAR(50) NOT NULL,
+	director VARCHAR(50) NOT NULL,
+	studio VARCHAR(50) NOT NULL,
+	active BOOLEAN,
+	PRIMARY KEY (idMovie)
+);
+
+CREATE TABLE TVShow(
+	idTVShow INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	season INT NOT NULL,
+	episodes INT NOT NULL,
+	genre VARCHAR(50) NOT NULL,
+	exibitionYear INT NOT NULL,
+	creator VARCHAR(50) NOT NULL,
+	channel VARCHAR(50) NOT NULL,
+	status BOOLEAN,
+	active BOOLEAN,
+	PRIMARY KEY (idTVShow)
+);
+
+CREATE TABLE User(
+	idUser INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	PRIMARY KEY (idUser)
+);
+
+CREATE TABLE User_Movies(
+	idUser INT NOT NULL,
+	idMovie INT NOT NULL,
+	watchList BOOLEAN,
+	rating INT NOT NULL, 
+	CONSTRAINT User_Movies_id_fk1 FOREIGN KEY (idUser) REFERENCES User(idUser),
+	CONSTRAINT User_Movies_id_fk2 FOREIGN KEY (idMovie) REFERENCES Movie(idMovie)
+);
+
+CREATE TABLE User_TVShow(
+	idUser INT NOT NULL,
+	idTVShow INT NOT NULL,
+	watchList BOOLEAN,
+	rating INT NOT NULL, 
+	CONSTRAINT User_Movies_id_fk1 FOREIGN KEY (idUser) REFERENCES User(idUser),
+	CONSTRAINT User_Movies_id_fk2 FOREIGN KEY (idTVShow) REFERENCES idTVShow(idTVShow)
+);
