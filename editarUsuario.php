f59a81fc689cd23bf70f16605bcf25750254abda
wesleyraul <42242383+wesleyraul@users.noreply.github.com>
2018-11-05 10:12:38 -0200

    Add files via upload

---
+++ editarUsuario.php
@@ -0,0 +1,31 @@
+<?php
+		require_once 'User.php';
+		require_once 'UserDAO.php';
+
+	  $banco_user = new UserDAO();
+	  if(isset($_POST["idUser"])){
+	    $user = $banco_user->buscarPorId($_POST["idUser"]);
+	  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <form action="UserHelper.php?acao=alterar" method="post">
+                
+        <input type="number" name="idUser" id="idUser" value="<?php echo $user->getIdUser() ?>" readonly ><br>
+        <input type="text" name="name" value="<?php echo $user->getNameUser()?>" required><br>
+
+        <div class="modal-footer">
+        <input type="submit" value="Editar" class="btn btn-success">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+    </form>
+	
+</body>
+
+
+</html>
